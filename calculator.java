93fac2cd76f70abcecdf1fc3c688d99c31d87da1
Erencan <erengcan@gmail.com>
2023-05-04 19:48:19 +0300

    Calculator eklendi.

---
+++ calculator.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class calculator {
+
+    public static void main(String[] args){
+
+
+        Scanner okuyucu = new Scanner(System.in) ;
+        System.out.println("hesap makinesine hoş geldiniz");
+        System.out.println("hesaplamak istediğiniz 2 sayıyı giriniz.") ;
+
+        int sayıBir= okuyucu.nextInt();
+        int sayıİki = okuyucu.nextInt();
+        System.out.println("hangi işlemi yapmak istersiniz? (*,/,-,+)");
+        char işlem = okuyucu.next().charAt(0);
+        int sonuç ;
+
+
+
+    switch (işlem) {
+
+
+
+        case '+' : sonuç =sayıBir+sayıİki;
+        System.out.println(sayıBir + "+" +sayıİki + "="+ sonuç);
+        break ;
+
+        case '-' : sonuç =sayıBir - sayıİki;
+            System.out.println(sayıBir + "-" +sayıİki + "="+ sonuç);
+            break ;
+        case '*' : sonuç =sayıBir*sayıİki;
+            System.out.println(sayıBir + "*" +sayıİki + "="+ sonuç);
+            break ;
+        case '/' : sonuç =sayıBir/sayıİki;
+            System.out.println(sayıBir + "/8" +sayıİki + "="+ sonuç);
+            break ;
+
+        default: System.out.println("Çarpma ,bölme ,çıkarma veya toplama seçiniz.");
+
+            }
+    okuyucu.close();
+
+
+
+
+
+
+
+    }
+}
